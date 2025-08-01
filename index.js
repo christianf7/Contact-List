c250b8852911c2e7382a89e30fdd6b63b6ae08b0
Christian F <christianfdev@gmail.com>
2025-08-01 15:14:50 +0000

    fix fetch

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const basicAuth = require('basic-auth');
-const fetch = require('node-fetch');
+const fetch = (...args) =>
+  import('node-fetch').then(mod => mod.default(...args));
 
 const app = express();
 const port = process.env.PORT || 3000;
